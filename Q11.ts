97cf4339f5e79a266b80ef88ae1f30c48e0844c6
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 11:28:20 -0700

    Q 11 ts/js node js of 45 assignment
    Q 11 ts/js node js of 45 assignment

---
+++ Q11.ts
@@ -0,0 +1,12 @@
+//Q11.Names: Store the names of a few of your friends in a array called names.
+ //Print each person’s name by accessing each element in the list, 
+ //one at a time.
+
+ let Names: string[] = ["Izna","AdvHaya" ,"Varsha" ,"Rekha", 
+ "Ravisha" ,"SONIA"]
+ console.log(Names[0]);
+ console.log(Names[1]);
+ console.log(Names[2]);
+ console.log(Names[3]);
+ console.log(Names[4]);
+ console.log(Names[5]);
